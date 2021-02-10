db66e4f8d3bdab8e3ad93479cbbf51f675d55680
MaimunSaniya <69858396+MaimunSaniya@users.noreply.github.com>
2021-02-10 14:00:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
 
 const image = {uri:'https://cutewallpaper.org/21/green-background-design-hd/Green-Pattern-Background-Wallpaper-Light-Designs-loliful.co.jpg'}
 
-import HomeScreen from './screens/HomeScreen';
-import SummaryScreen from './screens/SummaryScreen';
+import HomeScreen from './HomeScreen';
+import SummaryScreen from './SummaryScreen';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import AppHeader from './components/AppHeader';
+import AppHeader from './AppHeader';
 export default class App extends React.Component {
   render() {
     return (
